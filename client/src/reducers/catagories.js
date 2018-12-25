b5a9184e2e71fbd9488cee2a9c4beb9519d02c97
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-12-24 23:46:18 -0700

    change to reducers

--- client/src/reducers/catagories.js
+++ client/src/reducers/catagories.js
@@ -1,42 +1,42 @@
 import axios from 'axios'
 
-const CATAGORYS = 'CATAGORYS'
+const CATAGORIES = 'CATAGORIES'
 const ADD_CATAGORY = 'ADD_CATAGORY'
 const UPDATE_CATAGORY = 'UPDATE_CATAGORY'
 const DELETE_CATAGORY = 'DELETE_CATAGORY'
 
-export const getCatagorys = () => {
+export const getCatagories = () => {
   return (dispatch) => {
-    axios.get('/api/catagorys')
-      .then( res => dispatch({ type: CATAGORYS, catagorys: res.data }))
+    axios.get('/api/catagories')
+      .then( res => dispatch({ type: CATAGORIES, catagories: res.data }))
   }
 }
 
 export const addCatagory = (catagory) => {
   return (dispatch) => {
-    axios.post('/api/catagorys', {catagory} )
+    axios.post('/api/catagories', {catagory} )
       .then( res => dispatch({ type: ADD_CATAGORY, catagory: res.data }) )
   }
 }
 
 export const updateCatagory = (catagory) => {
   return (dispatch) => {
-    axios.put(`/api/catagorys/${$catagory.id}`, {catagory} )
+    axios.put(`/api/catagories/{$catagory.id}`, {catagory} )
       .then( res => dispatch({ type: UPDATE_CATAGORY, app: res.data }))
   }
 }
 
 export const deleteCatagory = (id) => {
   return (dispatch) => {
-    axios.delete(`/api/catagorys/${id}`)
+    axios.delete(`/api/catagories/${id}`)
       .then( () => dispatch({ type: DELETE_CATAGORY, id }))
   }
 }
 
 export default (state = [], action ) => {
   switch(action.type) {
-    case CATAGORYS:
-      return action.CATAGORYS
+    case CATAGORIES:
+      return action.CATAGORIES
     case ADD_CATAGORY:
       return [action.catagory, ...state]
     case UPDATE_CATAGORY:
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers, } from 'redux';
+import catagories from './catagories'
 
 
 const rootReducer = combineReducers({
+
+  catagories,
 
 });
 
